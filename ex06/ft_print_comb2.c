d0e32f4cdad76f78ab28bc0e20a41dea0f164cf7
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-03-03 20:55:15 +0000

    feat: Adds the folder 'ex06' and its file 'ft_print_comb2.c'.

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 13:24:39 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/03/03 21:00:18 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char string)
+{
+	write(1, &string, 1);
+}
+
+void	ft_show(int num)
+{
+	int	a;
+	int	b;
+
+	if (num > 9)
+	{
+		a = num / 10;
+		b = num % 10;
+		ft_putchar(a + '0');
+		ft_putchar(b + '0');
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(num + 48);
+	}
+}
+
+void	ft_result(int x, int y)
+{
+	ft_show(x);
+	ft_putchar(' ');
+	ft_show(y);
+	if (x < 98 || y < 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x <= 99)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			ft_result(x, y);
+			y++;
+		}
+		x++;
+	}
+}
